2eab3005b1a0db1d88505912067f6536286f1b2c
YukiKuroshima <yukikuro1993@gmail.com>
2017-10-05 22:29:31 -0700

    :tada:

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 from flask import Flask
 from datetime import datetime
 from time import sleep
+
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-    sleep_time = 10
+    sleep_time = randint(1, 7)
     sleep(sleep_time)
 
     return """
-    <h1>Hello heroku</h1>
     <p>I waited {0}[s].</p>
-
-    <img src="http://loremflickr.com/600/400">
     """.format(str(sleep_time))
 
 if __name__ == '__main__':
